7d572c8820ad98968aad53116358c4356679b034
Claire Illich <claire@gingerlabs.com>
2019-03-23 13:49:42 -0700

    Initial Commit

---
+++ HackVisualization/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HackVisualization
+//
+//  Created by Claire Illich on 3/23/19.
+//  Copyright © 2019 Claire Illich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
